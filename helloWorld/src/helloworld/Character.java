2be9de9ddd0dc6a9176f645fa45dae34449b4787
Owen King <tuj66158@temple.edu>
2021-11-07 17:31:28 -0500

    added constructors to Character and Enemy classes, still currently no differences

--- helloWorld/src/helloworld/Character.java
+++ helloWorld/src/helloworld/Character.java
@@ -1,4 +1,14 @@
 package helloworld;
 
 public class Character extends Battler{
+
+    public Character(){
+        super();
+    }
+
+    public Character(String name, int HP,int MaxHP, int MP, int MaxMP, int Atk, int Def){
+        super(name, HP, MaxHP, MP, MaxMP, Atk, Def);
+    }
+
+
 }
--- helloWorld/src/helloworld/Enemy.java
+++ helloWorld/src/helloworld/Enemy.java
@@ -1,4 +1,14 @@
 package helloworld;
 
 public class Enemy extends Battler{
+
+    public Enemy(){
+        super();
+    }
+
+    public Enemy(String name, int HP,int MaxHP, int MP, int MaxMP, int Atk, int Def){
+        super(name, HP, MaxHP, MP, MaxMP, Atk, Def);
+    }
+
+
 }
