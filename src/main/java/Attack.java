76ad2419f18bd5a5928dffaf4f1e7c80ba2db571
faith-ima <77637834+faith-ima@users.noreply.github.com>
2021-11-08 01:33:20 -0500

    implemented basic attcakk class

--- src/main/java/Attack.java
+++ src/main/java/Attack.java
@@ -1,3 +1,53 @@
 package main.java;
 public interface Attack {
-}
+    private int mpCosts;
+    private int numOfHits;
+    private String name;
+
+    public Attack(){
+        mpCosts = numOfHits = 0;
+        name = "";
+    }
+    public Attack(int mpCosts, int numOfHits, String name) {
+
+        this.mpCost = mpCost;
+        this.numOfHits = numOfHits;
+        this.name = name;
+    }
+
+    //getter and setter methods
+    public void setMpCosts(int mpCosts){
+        this.mpCosts = mpCosts;
+    }
+    public int getMpCosts(){
+        return mpCosts;
+    }
+    public void setNumOfHits(int numOfHits){
+        this.numOfHits = numOfHits;
+    }
+    public int getNumOfHits(){
+        return numOfHits;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public int calcDamage(Character a, Enemy b){ //assuming a is the characters current mp and b is the amount of damage
+        this.calcDamage = a - b;
+        if (this.calcDamage < 0) {
+            this.calcDamage = 0;
+        }
+    }
+    public boolean isUsableMp(){
+        if(mpCosts > 0)
+            return mpCosts;
+    }
+    public String getMessage(){
+        return "";
+    }
+
+
+}//end attack class
---
+++ src/main/java/specialAttack1.java
@@ -0,0 +1,13 @@
+package main.java;
+public class specialAttack1 extends Attack{
+
+    public specialAttack1(){
+        super();
+    }
+
+    public specialAttack1(int mpCosts, int numOfHits, String name){
+        super(mpCosts, numOfHits, name);
+    }
+
+
+}
