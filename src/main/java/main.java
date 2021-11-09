ec13bd3ad812d792f529a4eede626ac2bfc45a62
chaseglasper <tug68021@temple.edu>
2021-11-08 20:19:40 -0500

    Refactoring main.java
    getting rid of useless code

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -10,7 +10,7 @@ public class main {
 	static String charName;
 
 	public static void main(String[] args) {
-		System.out.println("This is to test if I can make this executable");
+		
 		Battler person1 = new Character();
 		Battler person2 = new Enemy();
 		
