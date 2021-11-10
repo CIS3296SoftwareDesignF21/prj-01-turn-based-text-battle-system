a014c6e7a67ad13c96b3c1cec02a48217d890376
Robert Morsa <tuj79353@temple.edu>
2021-11-10 14:00:41 -0500

    Added special attack to enemy in all constructors and two special attacks for the character within all constructors

--- src/main/java/Character.java
+++ src/main/java/Character.java
@@ -3,13 +3,19 @@ public class Character extends Battler{
 
     public Character(){
         super();
+        addSpecialAttack(new DualSlash());
+        addSpecialAttack(new Pierce());
     }
 
     public Character(String name, int HP,int MaxHP, int MP, int MaxMP, int Atk, int Def){
         super(name, HP, MaxHP, MP, MaxMP, Atk, Def);
+        addSpecialAttack(new DualSlash());
+        addSpecialAttack(new Pierce());
     }
 
     public Character(String name, int MaxHP, int MaxMP, int Atk, int Def){
         super(name, MaxHP, MaxMP, Atk, Def);
+        addSpecialAttack(new DualSlash());
+        addSpecialAttack(new Pierce());
     }
 }
--- src/main/java/Enemy.java
+++ src/main/java/Enemy.java
@@ -2,13 +2,16 @@ package main.java;
 public class Enemy extends Battler{
     public Enemy(){
         super();
+        addSpecialAttack(new Bash());
     }
 
     public Enemy(String name, int HP,int MaxHP, int MP, int MaxMP, int Atk, int Def){
         super(name, HP, MaxHP, MP, MaxMP, Atk, Def);
+        addSpecialAttack(new Bash());
     }
 
     public Enemy(String name, int HP, int MP, int Atk, int Def){
         super(name, HP, MP, Atk, Def);
+        addSpecialAttack(new Bash());
     }
 }
