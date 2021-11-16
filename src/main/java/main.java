ca52cf2600892f7cc229316395788603629e4375
Robert Morsa <tuj79353@temple.edu>
2021-11-15 20:34:43 -0500

    Restructed main. Loop statements have been added if invalid numbers are possible, setDifficulty() has new name and now sets the level to the user input with different messages and does not allow different numbers, messages have been changed a bit to sound better I think personally, messing up the number in the attack menu is now a feature, checkIfWon() is now checkIfFinished() and allows the user to continue fighting even if they won. the HP and MP for both the player and enemy are restored. Scanner has been added to everything because I worry for an error, added no answer to asking to play again

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -9,117 +9,98 @@ public class main {
 	static String userInput; // Hold user's String inputs
 	static int userInt; // Hold user's integers
 	static String charName;
-	static boolean won;
+	static boolean fin = false;
 	static Player player = null;
 	static Enemy enemy = null;
+	static Scanner sc = new Scanner(System.in);
 
 	public static void main(String[] args) {
+		System.out.println("Enter your name:");
+		charName = sc.nextLine();
 
-		System.out.println("Enter your name");
-		charName = stdin.nextLine();
-
-		run();
+		setDifficulty();
 		battleLoop();
-
 	}
 
-	static void run(){
-		System.out.println("How powerful is your character (1 - 3):");
-		userInt = stdin.nextInt();
-
-		switch (userInt) {
-			case 1:
-				player = Player.randomPlayer(1, charName);
-				break;
-			case 2:
-				player = Player.randomPlayer(2, charName);
-				break;
-			case 3:
-				player = Player.randomPlayer(3, charName);
-				break;
-			default:
-				System.out.println("Character level outside of range");
-				System.exit(-1);
-				break;
+	static void setDifficulty(){
+		//System.out.println("How powerful is your character (1 - 3):");
+		userInt = 0;
+		while(userInt > 3 || userInt < 1) {
+			System.out.println("Select your power level (1-3):");
+			userInt = sc.nextInt();
 		}
+		player = Player.randomPlayer(userInt, charName);
 
-		System.out.println("How strong of an enemy can you manage (1 - 3):");
-		userInt = stdin.nextInt();
-		switch (userInt) {
-			case 1:
-				enemy = Enemy.randomEnemy(1);
-				break;
-			case 2:
-				enemy = Enemy.randomEnemy(2);
-				break;
-			case 3:
-				enemy = Enemy.randomEnemy(3);
-				break;
-			default:
-				System.out.println("Enemy level outside of range");
-				System.exit(-1);
-				break;
+		//System.out.println("How strong of an enemy can you manage (1 - 3):");
+		userInt = 0;
+		while(userInt > 3 || userInt < 1) {
+			System.out.println("Select your enemy's power level (1-3)");
+			userInt = sc.nextInt();
 		}
+		enemy = Enemy.randomEnemy(userInt);
 	}
 
 	static private void battleLoop(){
-		won = false;
-		exit:
-		while(!won){
+		while(!fin){
 			while (player.getHP() > 0 && enemy.getHP() > 0) {
-				System.out.println("Enter 1 for attack. Enter 2 for Guard. Enter -1 to quit.\n ");
-				userInt = stdin.nextInt();
-
-				if (userInt == 1) {
-					player.useAction(enemy, "Attack");
-					if (enemy.getHP() > 0) {
-						enemy.useAction(player, "Attack");
-					}
-				} else if (userInt == 2) {
-					player.useAction(enemy, "Guard");
-					if (enemy.getHP() > 0) {
-						enemy.useAction(player, "Attack");
-					}
-				}else if(userInt == -1) {
-					System.out.print("You have quit");
-					System.exit(-1);
-				} else {
-					System.out.println("Invalid Entry\n");
+				System.out.println("Enter 1 for attack. Enter 2 for Guard. Enter -1 to quit.");
+				userInt = sc.nextInt();
+				switch(userInt){
+					case 1:
+						player.useAction(enemy, "Attack");
+						if (enemy.getHP() > 0) {
+							enemy.useAction(player, "Attack");
+						}
+						break;
+					case 2:
+						player.useAction(enemy, "Guard");
+						if (enemy.getHP() > 0) {
+							enemy.useAction(player, "Attack");
+						}
+						break;
+					case -1:
+						System.out.print(player.getName() + " fled the scene!\nThe battle is over!");
+						System.exit(1);
+						break;
+					default:
+						System.out.println(player.getName() + " fumbled and pressed an invalid number!");
+						if (enemy.getHP() > 0) {
+							enemy.useAction(player, "Attack");
+						}
+						break;
 				}
 				player.endTurn();
 				enemy.endTurn();
 			}
-			won = checkifwon();
-
+			fin = checkIfFinished();
 		}
 	}
 
-	private static boolean checkifwon() {
-		won = false;
+	private static boolean checkIfFinished() {
 		if(player.getHP() <= 0){
-			won = false;
-			System.out.println("You have been defeated D:\n ");
-
-			Scanner sc = new Scanner(System.in);
-
-			System.out.println("Do you want to play again?\n");
-			String response = sc.nextLine();
-			if(response.equals("yes")){
-				System.out.println("NEW ROUND!!!\n");
-				run();
-				battleLoop();
-			}
-			else{
-				System.out.println("Thank you for playing.\n");
-				System.exit(0);
-			}
-
-		} else {
-			won = true;
-			System.out.println("You have vanquished your foe :D\n Congratulations!\n");
-			System.exit(0);
+			System.out.println("You have been defeated D:");
+		} else if(enemy.getHP() <= 0) {
+			System.out.println("You have vanquished your foe :D\nCongratulations!");
 		}
-		return won;
-	}
+		else{
+			System.out.println("Something ended... I guess???");
+		}
 
+		while(true) {
+			System.out.println("Do you want to play again? Enter \"yes\" or \"no\".");
+			String response = sc.nextLine();
+			if (response.equals("yes")) {
+				System.out.println("NEW ROUND!!!\n");
+				player.recoverAll();
+				enemy.recoverAll();
+				fin = false;
+				break;
+			} else if (response.equals("no")) {
+				System.out.println("Thank you for playing.");
+				fin = true;
+				break;
+			}
+		}
+		return fin;
+	}
 }
